01a13838bf6759ade82e404d77cbea64c9b5c0c1
Rashidul <aboutrashidul@gmail.com>
2023-12-15 23:10:59 +0600

    login section JWT add

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../models/userModel");
 
+const SECRET_KEY = "MDRASHIDULISLAM";
 // Created Register
 router.post("/register", async (req, res) => {
   try {
@@ -43,7 +44,13 @@ router.post("/login", async (req, res) => {
     if (!isPasswordValid) {
       return res.status(401).json({ error: "Invalid Password" });
     }
-    res.status(201).json({ message: "User login successfully", user });
+
+    const token = jwt.sign({ userId: user._id }, SECRET_KEY, {
+      expiresIn: "1hr",
+    });
+    res
+      .status(201)
+      .json({ message: "User login successfully", user, data: token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error signing up" });
